73b08bbdb8d29cad98e2e1c01167aeb184bff95a
AltynaiGit <altynai.777.777.777@gmail.com>
2025-12-26 19:10:00 +0600

    ive finished swith case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,18 @@
-package org.example;
-
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Введите первое число-> ");
-        int firstNumber = sc.nextInt();
-        System.out.print("Введите второе число-> ");
-        int secontNumber = sc.nextInt();
-        System.out.print("Выберите операцию (+, -, *. /) ->");
-        String oper = sc.nextLine();
+ switch (oper) {
+            case "+":
+                System.out.println(firstNumber + secondNumber);
+                break;
+            case "-":
+                System.out.println(firstNumber - secondNumber);
+                break;
+            case "/":
+                System.out.println(firstNumber / secondNumber);
+                break;
+            case "*":
+                System.out.println(firstNumber * secondNumber);
+                break;
+            default:
+                System.out.println("Error");
+        }
     }
 }
